a632a2a056b7f3edb66b1365b5eaf4b454e606e1
clvsit <879646529@qq.com>
2020-10-30 23:00:22 +0800

    create model Label

--- recorder/models.py
+++ recorder/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Label(models.Model):
+    id = models.AutoField("标签编号", primary_key=True)
+    name = models.CharField("标签名称", unique=True, max_length=10)
+    brief = models.CharField("标签描述", default="", max_length=64)
+    date_create = models.DateTimeField("创建日期", auto_now_add=True)
+    date_modify = models.DateTimeField("修改日期", null=True)
+    spare1 = models.CharField("备用字段1", null=True, max_length=12)
+    spare2 = models.CharField("备用字段2", null=True, max_length=12)
+
+    def __str__(self):
+        return self.name
+
